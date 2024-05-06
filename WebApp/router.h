03f519aceb02ee7de849180e16c0555a7b1276e6
QuanQiuTong <fqt1550887523@outlook.com>
2024-05-06 18:26:33 +0800

    helper macro

--- WebApp/router.h
+++ WebApp/router.h
@@ -3,9 +3,27 @@
  * @author QuanQiuTong (fqt1550887523@outlook.com)
  * @brief A singleton class to build the router simply
  * @date 2024-04-27
- * 
+ *
  * @copyright Copyright (c) 2024
+ *
+ *
+ * The singleton class RouterBuilder is used to build the router simply--just like the snippet below:
  * 
+ * ```cpp
+ * #include "router.h"
+ * 
+ * INIT_BEGIN
+ * add_router({
+ *     make_path("/hello", GET, hello_handler),
+ *     make_path("/json", POST, json_handler),
+ * });
+ * INIT_END
+ * 
+ * boost::json::object json_handler( ... ) { ... }
+ * ```
+ * So you can add router configs in any file, and the router will be built before main function.
+ * Thus you can modularize your code and make it more readable.
+ *
  */
 
 #pragma once
@@ -22,13 +40,12 @@ public:
 	static RouterBuilder &getInstance()
 	{
 		static RouterBuilder instance; // Guaranteed to be destroyed.
-		return instance;               // Instantiated on first use.
+		return instance;			   // Instantiated on first use.
 	}
-	
 	Router _router;
 
 private:
-	RouterBuilder() {}                              // Private constructor
+	RouterBuilder() {}								// Private constructor
 	RouterBuilder(RouterBuilder const &) = delete;	// Delete copy constructor
 	void operator=(RouterBuilder const &) = delete; // Delete assignment operator
 };
@@ -41,3 +58,17 @@ void add_router(const Router &router)
 }
 
 Router get_router() { return RouterBuilder::getInstance()._router; }
+
+#define INIT_BEGIN   \
+	namespace        \
+	{                \
+		struct _init \
+		{            \
+			_init()  \
+			{
+/* Code here will be executed before main. */
+#define INIT_END  \
+	}             \
+	}             \
+	_before_main; \
+	}
